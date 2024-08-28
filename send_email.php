8ca0d107fd1208d03a33752da72e7874742da6c0
Abubakar-sidiq <kasabasidiq@gmail.com>
2024-08-28 13:55:34 +0100

    first commit

---
+++ send_email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "techsolinnovation693@gmail.com"; // Email address to send the form data
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message successfully sent!";
+    } else {
+        echo "Message sending failed!";
+    }
+}
+?>
